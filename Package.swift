c9dd88c1925ca7ccf8cfe541719ccb2f8ee1738d
Dmyro Romaniuk <ro.dmytro@gmail.com>
2017-01-25 22:09:43 +0200

    added xcodeproject

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "VaporApp",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
-        .Package(url: "https://github.com/bygri/vapor-forms", majorVersion: 1, minor: 0),
     ],
     exclude: [
         "Config",
